9eb9c06e29661c1bfaf8b83d9b31b4095589d93b
Ricky Tharrington <Ricky.Tharrington@sas.com>
2022-06-08 17:03:45 -0400

    feat: Adding magit

--- init.el
+++ init.el
@@ -1,7 +1,4 @@
-;; Make escape exit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-;; Packages
+; Packages
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -40,25 +37,93 @@
   :config
   (ivy-mode 1))
 
+;; Ivy Rich - Add Descriptions to Shortcuts
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
 ;; Counsel - Common Ivy Commands
-(use-package counsel)
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 ("C-M-j" . counsel-switch-buffer)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
+
+;; Space Bar
+(use-package general
+  :config
+  (general-create-definer rune/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (rune/leader-keys
+    "t" '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")
+    "tr" '(toggle-truncate-lines :which-key "truncate lines")
+    "ss" '(shell :which-key "shell")))
 
 ;; Vim Key Bindings
 (use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
   :config
-  (evil-mode 1))
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+  
+  ;; Use visual line motions even outside of the visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(rune/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale-text"))
 
 ;; Disable the Startup Message
 (setq inhibit-startup-message t)
 
-(scroll-bar-mode -1) ; Disable visible scrollbar
-(tool-bar-mode -1)   ; Disable the toolbar
-(tooltip-mode -1)    ; Disable tooltips
-(set-fringe-mode 10) ; Give some breathing room
-(menu-bar-mode -1)   ; Disable the menu bar
+(scroll-bar-mode -1)    ; Disable visible scrollbar
+(tool-bar-mode -1)      ; Disable the toolbar
+(tooltip-mode -1)       ; Disable tooltips
+(set-fringe-mode 10)    ; Give some breathing room
+(menu-bar-mode -1)      ; Disable the menu bar
+(toggle-truncate-lines) ; Disable line wrapping
+(set-language-environment "UTF-8") ; Emojis
 
 ;; Set up the Visible Bell
 (setq visible-bell t)
+
+;; Display Line Numbers
+(column-number-mode)
+(global-display-line-numbers-mode t)
+(menu-bar--display-line-numbers-mode-relative)
+
+;; Disable Line Numbers for Some Modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; Mode Line
 (use-package doom-modeline
@@ -67,6 +132,54 @@
   :custom ((doom-modeline-height 15)))
 
 ;; Themes/Fonts
+(use-package doom-themes
+  :init (load-theme 'doom-tomorrow-night t))
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 120)
-(load-theme 'wombat)
 
+;; Rainbow Delimiters
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Which-Key (shortcut hints)
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Helpful - Better function descriptions
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;; Project Helpers
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (setq projectile-project-search-path '("~/"))
+  (when (file-directory-p "~/projects")
+    (add-to-list 'projectile-project-search-path '"~/projects"))
+  (when (file-directory-p "~/MLPA")
+    (add-to-list 'projectile-project-search-path '"~/MLPA"))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+;; Git Tools
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package evil-magit
+  :after magit)
---
+++ run-emacs.sh
@@ -0,0 +1,1 @@
+emacs -q -l ./init.el
