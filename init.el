e4cf441dcbf40d29503a0f26a65a97dcd8204636
Ricky Tharrington <ricky.tharrington@gmail.com>
2022-06-22 20:20:57 -0400

    Remove evil shift right comment

--- init.el
+++ init.el
@@ -86,7 +86,6 @@
   (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-;;  (define-key evil-insert-state-map (kbd "TAB") 'evil-shift-right)
 
   ;; Use visual line motions even outside of visual-line-mode buffers
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
